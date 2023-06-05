df9678c7126d4840bd895d107db5a20c1f2aa0dc
it <it@DESKTOP-13155GB>
2023-06-05 14:56:52 +0900

    3장 16번까지

---
+++ ex02_problem/src/ex03/problem/EX12.java
@@ -0,0 +1,27 @@
+package ex03.problem;
+
+public class EX12 
+{
+	public static void main(String[] args) 
+	{
+		if(args.length == 0)
+		{
+			System.out.println("명령행 인자(arguments)가 없어요");
+			return;
+		}
+		
+		int sum=0;
+		for(int i=0;i<args.length;++i) 
+		{
+			try {
+				sum = sum + Integer.parseInt(args[i]);
+			}
+			catch(Exception e){ }
+		}
+		
+		System.out.println(sum);
+	}
+}
+
+
+
---
+++ ex02_problem/src/ex03/problem/EX16.java
@@ -0,0 +1,64 @@
+package ex03.problem;
+
+import java.util.Scanner;
+
+public class EX16 
+{
+	public static void main(String[] args) 
+	{
+        Scanner sc1 = new Scanner(System.in);
+        
+        System.out.println("가위 바위 보 게임을 시작합니다.");
+        while(true)
+        {
+        	System.out.print("가위 바위 보!>>");
+        	String me = sc1.next();
+        	
+        	if(me.equals("그만"))
+        	{
+        		System.out.println("게임을 종료합니다.");
+        		break;
+        	}
+        	
+        	int n = (int)(Math.random()*3); //컴퓨터가 낸것을 랜덤하게 선택
+        	String com1[] = {"가위", "바위", "보"};
+        	String com = com1[n];
+        	
+        	if(me.equals("가위"))
+        	{
+        		if(com.equals("가위"))
+        			System.out.println("사용자="+ me+", 컴퓨터="+com+", 비겼습니다.");
+        		else if(com.equals("바위"))
+        			System.out.println("사용자="+ me+", 컴퓨터="+com+", 컴퓨터가 이겼습니다.");
+        		else if(com.equals("보"))
+        			System.out.println("사용자="+ me+", 컴퓨터="+com+", 사용자가 이겼습니다.");
+        	}
+        	else if(me.equals("바위"))
+        	{
+        		if(com.equals("가위"))
+        			System.out.println("사용자="+ me+", 컴퓨터="+com+", 사용자가 이겼습니다.");
+        		else if(com.equals("바위"))
+        			System.out.println("사용자="+ me+", 컴퓨터="+com+", 비겼습니다");
+        		else if(com.equals("보"))
+        			System.out.println("사용자="+ me+", 컴퓨터="+com+", 컴퓨터가 이겼습니다.");
+        	}
+        	else if(me.equals("보"))
+        	{
+        		if(com.equals("가위"))
+        			System.out.println("사용자="+ me+", 컴퓨터="+com+", 컴퓨터가 이겼습니다.");
+        		else if(com.equals("바위"))
+        			System.out.println("사용자="+ me+", 컴퓨터="+com+", 사용자가 이겼습니다.");
+        		else if(com.equals("보"))
+        			System.out.println("사용자="+ me+", 컴퓨터="+com+", 비겼습니다.");
+        	}
+        	else
+        		System.out.println("가위 바위 보 중에서 입력해주세요!");
+        	
+        }
+		
+		
+		sc1.close();
+	}
+}
+
+
