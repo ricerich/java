1b63225192f8af6977f949db66be05587dbaa34e
it <it@DESKTOP-13155GB>
2023-06-07 10:12:57 +0900

    객체지향 파일 3개

--- ex02_problem/src/OOP/EX01_BASIC.java
+++ ex02_problem/src/OOP/EX01_BASIC.java
@@ -4,18 +4,8 @@ public class EX01_BASIC
 {
 	public static void main(String[] args) 
 	{
-		Student s1 = new Student();
-		
-		System.out.println(s1.name);
-		
-		s1.name = "이순신";
-		
-		System.out.println(s1.name);
-		
-		s1.name = "강개토";
-		
-		String str1 = s1.getName();
-		System.out.println(str1);
+		StudentManager sm1 = new StudentManager();
+		sm1.run();
 	}
 
 }
---
+++ ex02_problem/src/OOP/StudentManager.java
@@ -0,0 +1,20 @@
+package OOP;
+
+public class StudentManager 
+{
+	void run() 
+	{
+		Student s1 = new Student();
+		
+		System.out.println(s1.name);
+		
+		s1.name = "이순신";
+		
+		System.out.println(s1.name);
+		
+		s1.name = "강개토";
+		
+		String str1 = s1.getName();
+		System.out.println(str1);
+	}
+}
