9ed83547e17cd96ca84be22dce0c4ab099bc8b9e
it <it@DESKTOP-13155GB>
2023-06-20 15:18:12 +0900

    9장 1번

---
+++ ex02_problem/src/ex09/EX09_01.java
@@ -0,0 +1,9 @@
+package ex09;
+
+public class EX09_01 
+{
+	public static void main(String[] args) {
+		new MyFrame();
+	}
+
+}
---
+++ ex02_problem/src/ex09/MyFrame.java
@@ -0,0 +1,13 @@
+package ex09;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame
+{
+	public MyFrame()
+	{
+		setTitle("타이틀에 제목적는 곳");
+		setSize(500, 300);
+		setVisible(true);
+	}
+}
